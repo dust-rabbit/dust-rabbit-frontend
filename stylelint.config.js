0bd07f942a7c2432ad81dd27e3241818e1894a8e
daejlee <savif4991@gmail.com>
2025-01-24 20:29:43 +0900

    Style: Rearrange styles

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  extends: ["stylelint-config-standard", "stylelint-config-recommended-scss"],
+  files: ["**/*.scss"],
   rules: {},
+  "stylelint.validate": ["css", "scss"],
 };
