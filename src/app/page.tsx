ba32d257fde42051d892c3f4927baf4d9aae2dd2
daejlee <savif4991@gmail.com>
2025-01-26 00:31:08 +0900

    feat: 추가 컴포넌트 루트 페이지에 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,20 @@
 
 import { OnboardingNavbar } from "@/components/shared";
 import { useProgress } from "@/hooks";
+import { Button, PageBackButton } from "@/components/ui";
 
 export default function Home() {
   const { progress, incrementProgress } = useProgress({ maxProgress: 4 });
 
-  return <OnboardingNavbar progress={progress} incrementProgress={incrementProgress} />;
+  return (
+    <>
+      <OnboardingNavbar progress={progress} incrementProgress={incrementProgress} />
+      <div className="row">
+        <Button>여자</Button>
+        <Button>남자</Button>
+      </div>
+      <PageBackButton />
+      <Button>다음</Button>
+    </>
+  );
 }
