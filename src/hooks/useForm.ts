6c4abaaeb83aa224ddf06cd15ad7b383f9baff07
donghyun kim <donghyun1998@naver.com>
2025-02-02 22:44:37 +0900

    Update src/hooks/useForm.ts
    Co-authored-by: Lee Daejin <savif4991@gmail.com>

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,17 +1,20 @@
 import { useState } from "react";
 
-type UseFormReturn<T extends Record<string, any>> = {
+type UseFormReturn<T extends Record<string, string | undefined>> = {
   form: T;
-  handleChange: (key: keyof T, value: any) => void;
+  handleChange: (key: keyof T, value: string | undefined) => void;
   setForm: React.Dispatch<React.SetStateAction<T>>;
 };
 
-export const useForm = <T extends Record<string, any>>(initialForm: T): UseFormReturn<T> => {
+export const useForm = <T extends Record<string, string | undefined>>(
+  initialForm: T,
+): UseFormReturn<T> => {
   const [form, setForm] = useState<T>(initialForm);
 
-  const handleChange = (key: keyof T, value: any) => {
+  const handleChange = (key: keyof T, value: string | undefined) => {
     setForm((prev) => ({ ...prev, [key]: value }));
   };
+
 
   return {
     form,
