065612ed6e1b14975a9b2c0d2f78cf2c51f55f3f
donghyun <donghyun1998@naver.com>
2025-02-09 13:53:11 +0900

    feat: datePicker 기획 변경으로 삭제

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,5 @@
 export * from "./button/button";
 export * from "./button/page-back-button";
 export * from "./button/x-button";
-export * from "./date-picker/date-picker";
 export * from "./progress-bar/progress-bar";
 export * from "./progress-indicator/progress-indicator";
