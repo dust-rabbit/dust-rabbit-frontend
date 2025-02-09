2e5ba219718048494a8a4df3c59bb49ca4165473
daejlee <savif4991@gmail.com>
2025-02-09 21:35:24 +0900

    chore: 기존 pr과 merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Form, OnboardingNavbar, MbtiCardContainer, TypeBirthDateStep } from "@/components/shared";
-import { Form, OnboardingNavbar, TypeBirthDateStep } from "@/components/shared";
 import { Button } from "@/components/ui";
 import { useForm, useFunnel } from "@/hooks";
 import { DEFAULT_FORM, SIGN_UP_STEPS } from "@/lib/const";
