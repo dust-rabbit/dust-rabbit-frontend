fe4b445596244a6b0909cce8d3de7ae79ec36e69
daejlee <savif4991@gmail.com>
2025-02-14 21:30:21 +0900

    style: button css 변경

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,13 +1,23 @@
 import styles from "./styles.module.scss";
 
+type ButtonVariants = "primary" | "disabled";
+
 type Props = {
   children: string;
   onClick?: () => void;
+  variant?: ButtonVariants;
 };
 
-export function Button({ children, onClick }: Readonly<Props>) {
+export function Button({ children, onClick, variant = "primary" }: Readonly<Props>) {
+  if (variant === "primary") {
+    return (
+      <button type="button" className={styles.primary} onClick={onClick}>
+        <h5>{children}</h5>
+      </button>
+    );
+  }
   return (
-    <button type="button" className={styles.default} onClick={onClick}>
+    <button type="button" className={styles.disabled}>
       <h5>{children}</h5>
     </button>
   );
