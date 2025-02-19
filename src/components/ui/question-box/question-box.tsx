56e965cb598c2a69937a31b47b608702b5c15850
daejlee <savif4991@gmail.com>
2025-02-19 10:59:11 +0900

    feat: QUESTION 상수 리스트화

--- src/components/ui/question-box/question-box.tsx
+++ src/components/ui/question-box/question-box.tsx
@@ -1,7 +1,5 @@
-import bubble from "@/assets/bubble.png";
 import { QUESTIONS } from "@/lib/const";
 import { SignUpStep } from "@/type";
-import Image from "next/image";
 import styles from "./styles.module.scss";
 
 type Props = {
@@ -11,8 +9,12 @@ type Props = {
 export function QuestionBox({ step }: Props) {
   return (
     <div className={styles["question-box"]}>
-      <Image src={bubble} alt="bubble" className={styles.bubble} />
-      <h1 className={styles.question}>{QUESTIONS[step]}</h1>
+      {/* <Image src={bubble} alt="bubble" className={styles.bubble} /> */}
+      {QUESTIONS[step].map((question) => (
+        <h1 key={question} className={styles.question}>
+          {question}
+        </h1>
+      ))}
     </div>
   );
 }
