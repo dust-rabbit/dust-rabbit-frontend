0266b8ab53747621415f299a74305aae122bafb9
donghyun <donghyun1998@naver.com>
2025-02-20 15:11:01 +0900

    feat: add validation schema

---
+++ src/lib/const/validationSchema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export const ValidationSchema = {
+  typeName: z.object({
+    name: z
+      .string()
+      .min(2, "2~6글자, 한글만 입력 가능해요")
+      .max(6, "2~6글자, 한글만 입력 가능해요")
+      .regex(/^[가-힣]+$/, "한글만 입력 가능해요"),
+  }),
+  typeBirthDate: z.object({
+    birthDate: z.string().refine((date) => {
+      const [year, month, day] = date.split(".").map(Number);
+      const inputDate = new Date(year, month - 1, day);
+      const currentYear = new Date().getFullYear();
+
+      const isValidDate =
+        inputDate.getFullYear() === year &&
+        inputDate.getMonth() === month - 1 &&
+        inputDate.getDate() === day;
+
+      const isValidYear = year >= 1900 && year <= currentYear;
+
+      return isValidDate && isValidYear;
+    }, "올바른 생년월일을 입력해주세요."),
+    calendarType: z.enum(["양력", "음력"]),
+  }),
+  birthTimeStep: z.object({
+    birthTime: z.string().refine((time) => {
+      const [hours, minutes] = time.split(":").map(Number);
+      return hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59;
+    }, "올바른 시간을 입력해주세요."),
+  }),
+};
