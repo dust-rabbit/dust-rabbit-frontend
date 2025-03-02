4177da82f1fddbb93b8d91bfd64f0cac5ab05bb5
daejlee <savif4991@gmail.com>
2025-03-02 16:51:42 +0900

    feat: add review info step

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import munto from "@/assets/mt-question.png";
 import { OnboardingNavbar, StepContainer } from "@/components/shared";
-import { Bubble } from "@/components/ui";
 import { FormProvider, useForm, useFunnel } from "@/hooks";
-import { DEFAULT_FORM, QUESTIONS, SIGN_UP_STEPS } from "@/lib/const";
+import { DEFAULT_FORM, SIGN_UP_STEPS } from "@/lib/const";
 import { SignUpForm, SignUpStep } from "@/type";
-import Image from "next/image";
 
 export default function SignUp() {
   const { currentStep, currentStepIndex, goToPreviousStep, goToNextStep } =
@@ -21,8 +18,6 @@ export default function SignUp() {
         totalSteps={SIGN_UP_STEPS.length}
         goToPreviousStep={goToPreviousStep}
       />
-      <Bubble speech={QUESTIONS[currentStep]} />
-      <Image src={munto} alt="munto" />
       <StepContainer
         currentStep={currentStep}
         form={form}
---
+++ src/components/shared/sign-up-step/step/review-info-step.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { SignUpForm } from "@/type";
+import { Button } from "@/components/ui";
+import munto from "@/assets/mt-question.png";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+type Props = {
+  form: SignUpForm;
+  onSubmit?: () => void;
+};
+
+const PERSONALITY = {
+  ISTJ: "체계적인 성향",
+  ISFJ: "헌신적인 성향",
+  INFJ: "이상주의적인 성향",
+  INTJ: "분석적인 성향",
+  ISTP: "냉철한 성향",
+  ISFP: "온화한 성향",
+  INFP: "공감적인 성향",
+  INTP: "논리적인 성향",
+  ESTP: "직설적인 성향",
+  ESFP: "활발한 성향",
+  ENFP: "열정적인 성향",
+  ENTP: "도전적인 성향",
+  ESTJ: "단호한 성향",
+  ESFJ: "화합적인 성향",
+  ENFJ: "사람 중심적인 성향",
+  ENTJ: "주도적인 성향",
+};
+
+// TODO: MBTI 타입이랑 타입체커 구현
+
+export function ReviewInfoStep({ form }: Readonly<Props>) {
+  const name = `${form.name}${form.gender === "남자" ? "군" : "양"}`;
+  const birthDate = form.birthDate.split(".");
+  const birth = `${birthDate[0]}년 ${Number(birthDate[1])}월 ${Number(birthDate[2])}일${form.birthTime ? ` ${form.birthTime}` : ""}`;
+  const { mbti } = form;
+  const personality = PERSONALITY[mbti as keyof typeof PERSONALITY];
+
+  return (
+    <div className={styles.container}>
+      <span>
+        <p>반가워요 {name}!</p>
+        <p>저는 점 봐주는 토끼, 점토에요.</p>
+        <p>{birth}에 태어나셨군요.</p>
+        <p>
+          {mbti}라니 {personality}을 지니셨겠어요.
+        </p>
+        <p>제가 {name}을 제대로 이해했나요?</p>
+        <p>시작하기 버튼을 누르면 매일 운세를 봐드릴게요.</p>
+      </span>
+      <Image src={munto} alt="munto" />
+      <Button>시작하기</Button>
+    </div>
+  );
+}
