6ec98970377c42b4bc59f554784baa44ffb114da
daejlee <savif4991@gmail.com>
2025-03-04 01:29:57 +0900

    feat: add step consts

--- src/components/shared/onboarding-navbar/onboarding-navbar.tsx
+++ src/components/shared/onboarding-navbar/onboarding-navbar.tsx
@@ -12,19 +12,23 @@ type Props = {
 export function OnboardingNavbar({ stepIndex, totalSteps, goToPreviousStep }: Readonly<Props>) {
   const { currentStepFormRef, isFormValid } = useFormContext();
   const progress = stepIndex + 1;
+  const isFirstStep = stepIndex === 0;
+  const isLastStep = stepIndex === totalSteps - 1;
+  const isSigninStep = stepIndex < totalSteps - 1;
+  const isProgressedStep = stepIndex > 0;
 
   return (
     <div className={styles.container}>
       <div className={styles["back-forward-container"]}>
-        {stepIndex === 0 && <div />}
-        {stepIndex > 0 && <BackButton onClick={goToPreviousStep} />}
-        {stepIndex < totalSteps - 1 && (
+        {isFirstStep && <div />}
+        {isProgressedStep && <BackButton onClick={goToPreviousStep} />}
+        {isSigninStep && (
           <ForwardButton
             onClick={() => submitRefForm(currentStepFormRef)}
             disabled={!isFormValid}
           />
         )}
-        {stepIndex === totalSteps && <div />}
+        {isLastStep && <div />}
       </div>
       <ProgressBar
         className={styles["progress-bar"]}
